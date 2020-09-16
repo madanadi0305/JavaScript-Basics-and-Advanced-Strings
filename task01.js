803a41050cca1b9ad1bec067e59e7e3c83ff11a6
madanadi0305 <madanadi0305@gmail.com>
2020-09-16 16:55:25 +0000

    Update task01.js

--- task01.js
+++ task01.js
@@ -2,7 +2,7 @@ const  checkStringLength=(inputString)=>{
 var string1=inputString.split("");
 var length;
 if(string1===""||string1===" " ){
-length=0;
+length="zero";
 }
 else{
     length=string1.length;

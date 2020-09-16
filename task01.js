df5ecebd28dbd6951b663a166c5c3d50648af8c7
madanadi0305 <madanadi0305@gmail.com>
2020-09-16 17:02:44 +0000

    Update task01.js

--- task01.js
+++ task01.js
@@ -2,13 +2,13 @@ const  checkStringLength=(inputString)=>{
 var string1=inputString.split("");
 var length;
 if(string1===""||string1===" " ){
- return 0;
+ return "zero";
 }
 else{
     length=string1.length;
     return length;
 }
 
-}
+};
 
 module.exports=checkStringLength;

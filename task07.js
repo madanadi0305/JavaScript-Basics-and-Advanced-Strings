ee9ad803242f2433fd9d9551f35b7796ba7acc7c
madanadi0305 <madanadi0305@gmail.com>
2020-09-17 16:30:36 +0000

    Update task07.js

---
+++ task07.js
@@ -0,0 +1,20 @@
+function capitaliseFirstLetter(string){
+    split_string=string.split(" ");
+    var k;
+    var result_string="";
+    for(var i=0;i<split_string.length;i++){
+        k=split_string[i];
+        for(var j=0;j<k.length;j++){
+            if(j===0){
+            result_string=result_string+k[j].toUpperCase();
+            }
+            else{
+                result_string=result_string+k[j];
+            }
+        }
+     result_string=result_string+" ";   
+    }
+    return result_string;
+}
+
+module.exports=capitaliseFirstLetter;

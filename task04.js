ab8db2843a014339ed6d7a575b611fbc6931cb93
madanadi0305 <madanadi0305@gmail.com>
2020-09-17 12:21:43 +0000

    Update task04.js

--- task04.js
+++ task04.js
@@ -5,11 +5,8 @@ const HiddenEmailId=(emailId)=>{
  var email_sub_string2=email_split[1];
  var sub_string1="";
  sub_string1=sub_string1+email_sub_string1.slice(0,4);
- for(var i=4;i<email_sub_string1.length;i++){
-     sub_string1=sub_string1+".";
-
- }
-sub_string1=sub_string1+"@"+email_sub_string2;
+ 
+sub_string1=sub_string1+"...."+"@"+email_sub_string2;
  return sub_string1;
 };
 module.exports=HiddenEmailId;

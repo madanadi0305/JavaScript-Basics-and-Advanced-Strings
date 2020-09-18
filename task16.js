075ce4657f68b77dd615dc0d7016238a3de458d2
madanadi0305 <madanadi0305@gmail.com>
2020-09-18 18:08:49 +0000

    Update task16.js

--- task16.js
+++ task16.js
@@ -4,11 +4,10 @@ var string1=string.split("")
 var result=[];
 for(var i=0;i<string1.length;i++){
  if(string1[i]==='a'||string1[i]==='e'||string1[i]==='i'||string1[i]==='o'||string1[i]==='u') {
-result[i]+=string1[i].toUpperCase();
-
+result.push(string1[i].toUpperCase());
  }
  else{
-     result[i]+=string1[i].toLowerCase();
+     result.push(string1[i].toLowerCase());
  }  
 }
 var result_final=result.join("");

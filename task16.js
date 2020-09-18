cae637531ecd911db32e9fca6d22b348c56a4e79
madanadi0305 <madanadi0305@gmail.com>
2020-09-18 17:46:49 +0000

    Update task16.js

---
+++ task16.js
@@ -0,0 +1,17 @@
+function capitaliseVowels(string){
+var string1=string.split("")
+;
+var result=[];
+for(var i=0;i<string1.length;i++){
+ if(string1[i]==='a'||string1[i]==='e'||string1[i]==='i'||string1[i]==='o'||string1[i]==='u') {
+result[i]+=string1[i].toUpperCase();
+
+ }
+ else{
+     result[i]+=string1[i].toLowerCase();
+ }  
+}
+var result_final=result.join("");
+
+}
+module.exports=capitaliseVowels;

4016acd9ea9d22bd18294fdacad12ac21b7f741e
madanadi0305 <madanadi0305@gmail.com>
2020-09-18 08:26:57 +0000

    Update Wiki/26-task14.md, task14.js files

---
+++ task14.js
@@ -0,0 +1,21 @@
+function returnPartOfString(string,character,flag){
+var string1=string.split("");
+var result_string="";
+var string2=string.split("&");
+for(var i=0;i<string1.length;i++){
+    if(string1[i]==character){
+     if(flag===1){
+         result_string=result_string+string2[0];
+         break;
+     }   
+     else if(flag==2){
+         result_string=result_string+string2[1];
+         break;
+     }
+    }
+    
+}
+    
+return result_string;    
+}
+module.exports=returnPartOfString;

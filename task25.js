fcea011480c31d3a818fcd62c53fb32b040bfd88
madanadi0305 <madanadi0305@gmail.com>
2020-09-19 12:44:46 +0000

    Update task25.js

--- task25.js
+++ task25.js
@@ -1,7 +1,7 @@
 const multiplyString=(string1,string2)=>{
 var product_string="";
 
-product_string=(parseInt(string1)*parseInt(string2)).toString(2);
+product_string=(parseInt(string1,2)*parseInt(string2,2)).toString(2);
 return product_string;
 };
 module.exports=multiplyString;

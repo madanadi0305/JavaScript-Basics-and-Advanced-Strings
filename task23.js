0d0e50e73b9d9665af0b2ade8e6f76629c12e80a
madanadi0305 <madanadi0305@gmail.com>
2020-09-19 14:46:58 +0000

    Update task23.js

--- task23.js
+++ task23.js
@@ -2,9 +2,16 @@ const incrementMessage=(string,number)=>{
 var val;
 var str2="";
 var str1=[];
+
 for(var i=0;i<string.length;i++)
-{ val=string.charCodeAt(i)+number;
+{ val=string.charCodeAt(i);
+  if(val===" "){
+     continue;
+  }
+  else{
+     val=val+number;
    str1.push(val);
+  }
 }
 str2=str2+String.fromCharCode.apply(null,str1);
 return str2;

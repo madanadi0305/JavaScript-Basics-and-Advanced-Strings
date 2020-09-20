416a8554d1cb830a7134bb1b5fbb245e3ca57c35
madanadi0305 <madanadi0305@gmail.com>
2020-09-20 13:11:04 +0000

    Update task17.js

--- task17.js
+++ task17.js
@@ -1,15 +1,15 @@
 function checkLastSubString(string,substring){
     var string1=string.split(" ");
     var is_Found;
-    if(substring=" "){
-      return false;
-    }
+   // if(substring=" "){
+     // return false;
+    //}
     for(var i=0;i<string1.length;i++){
       is_Found=0;
       if(i===(string1.length-1)){
         if(string1[i]===substring){
          is_Found=1; 
-          break;
+          //break;
         }
         
       }

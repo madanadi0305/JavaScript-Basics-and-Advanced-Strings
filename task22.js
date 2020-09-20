69b1bbc95d813e8605a32068fc0e03deb0dff3db
madanadi0305 <madanadi0305@gmail.com>
2020-09-20 07:23:42 +0000

    Update task22.js

--- task22.js
+++ task22.js
@@ -4,11 +4,14 @@ var is_Found;
 var index=string.indexOf(substring);
 while(true){
   is_Found=0;
-if(index!==-1){
-is_Found=1;
-break;
+if(index===-1){
+is_Found=0;
+//break;
 }
-
+else{
+  is_Found=1;
+}
+break;
 }
 if(is_Found===1){
   return true;

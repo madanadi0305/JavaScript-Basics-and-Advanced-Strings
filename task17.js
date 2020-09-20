de5b24ff20f0b780e83bc4f7042afc2265fd7ef0
madanadi0305 <madanadi0305@gmail.com>
2020-09-20 13:28:45 +0000

    Update task17.js

--- task17.js
+++ task17.js
@@ -7,17 +7,17 @@ function checkLastSubString(string,substring){
     for(var i=0;i<string1.length;i++){
       is_Found=0;
       if(i===(string1.length-1)){
-        if(string[i]===" "){
-          is_Found=0;
-        }
+        
         if(string1[i]===substring){
          is_Found=1; 
           //break;
         }
-        
+        else if(string1.indexOf(substring,string1.length-substring.length)!==-1){
+        is_Found=2;
+        }
       }
     }
-   if(is_Found===1){
+   if(is_Found===1||is_Found===2){
      return true;
    } 
     else{

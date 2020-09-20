bc1c636ad25c6e0f7d445f0d94e8811b069207a3
madanadi0305 <madanadi0305@gmail.com>
2020-09-20 07:15:22 +0000

    Update task22.js

--- task22.js
+++ task22.js
@@ -1,13 +1,20 @@
 const findSubString=(string,substring)=>{
-//var is_Found;
-var index=string.indexOf(string);
+var is_Found;
+
+var index=string.indexOf(substring);
+while(true){
+  is_Found=0;
 if(index!==-1){
+is_Found=1;
+break;
+}
+
+}
+if(is_Found===1){
   return true;
 }
-if(substring===" "){return false;}
 else{
   return false;
 }
-
 };
 module.exports=findSubString;

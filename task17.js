72cb15b073f379f7a465cd186bb1a3115b4e9f4f
madanadi0305 <madanadi0305@gmail.com>
2020-09-21 07:40:28 +0000

    Update Wiki/29-task17.md, task17.js files

--- task17.js
+++ task17.js
@@ -1,31 +1,41 @@
-function checkLastSubString(string,substring){
-    var string1=string.split(" ");
-    var is_Found;
-    if(substring=""||substring===null||string===null||string===""){
-      is_Found=0;
-      return false;
-    }
-    else{
-    for(var i=0;i<string1.length;i++){
-      is_Found=0;
-      if(i===(string1.length-1)){
-        
-        if(string1[i]===substring){
-         is_Found=1; 
-          //break;
-        }
-        else if(string1.indexOf(substring,string.length-substring.length)!==-1){
-        is_Found=2;
-        }
-      }
-    }
-   if(is_Found===1||is_Found===2){
-     return true;
-   } 
-    else{
-      return false;
-    }
-    }
+function endsWithSubstring(string,substring){
+var string1=string.split(" ");
+  var str2=[];
+  var is_Found;
+  if(string===null||string===""||substring===null||substring===""){
+  return false;
   }
+  
+  var n=string1.length;
+for(var i=0;i<n;i++){
+is_Found=0;
+  if(i===n-1){
+var k=string1[i];
+var last_index=k.length-1;
+  
+for(var j=last_index;j>(last_index-substring.length);j--)  
+str2.push(k[j]);
 
-  module.exports=checkLastSubString;
+}
+
+  var str3=str2.reverse().join("");
+
+   if(str3.toUpperCase()===substring.toUpperCase()||str3.toLowerCase()===substring.toLowerCase()){
+   is_Found=1;
+   break;
+   }
+  
+} 
+ if(is_Found===1){
+ return true;
+ } 
+else{
+return false;
+}
+  
+
+}
+
+module.exports=endsWithSubstring;
+
+

781cf8a06c789a2c25e58be5d43fa2cd83e07935
madanadi0305 <madanadi0305@gmail.com>
2020-09-21 16:57:36 +0000

    Update task23.js

--- task23.js
+++ task23.js
@@ -6,9 +6,9 @@ var ascii_code=string.charCodeAt(i)+number;
 if((ascii_code>=97 && ascii_code<=122)||(ascii_code<=90 && ascii_code>=65)){
 str2.push(String.fromCharCode(ascii_code));
 }  
-   /*else if(string[i]===" "){
+   else if(string[i]===" "){
    str2.push(" ");
-   }*/
+   }
  else{continue;}  
 }
 return str2.join("");

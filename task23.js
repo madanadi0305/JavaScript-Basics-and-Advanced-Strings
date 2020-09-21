ae50848d8e13955f3956224b4541ea09e9265423
madanadi0305 <madanadi0305@gmail.com>
2020-09-21 15:07:31 +0000

    Update task23.js

--- task23.js
+++ task23.js
@@ -1,22 +1,16 @@
-const incrementMessage=(string,number)=>{
-var val;
-var elem;
-var str1=[];
+function incrementMessage(string,number){
 var str2=[];
-for(var i=0;i<string.length;i++){
-if(string[i]===" "){
-  //continue;
-  str1.push(" ");
-}
-else{
-str1.push(string.charCodeAt(i)+number);
-}
-}
-
-for(var i=0;i<str1.length;i++){
-  elem=String.fromCharCode(str1[i]);
-  str2.push(elem);
+ for(var i=0;i<string.length;i++){
+//var k=string[i];]
+var ascii_code=string.charCodeAt(i)+number;
+if((ascii_code>=97 && ascii_code<=122)||(ascii_code<=90 && ascii_code>=65)){
+str2.push(String.fromCharCode(ascii_code));
+}  
+   else if(string[i]===" "){
+   str2.push(" ");
+   }
+ else{continue;}  
 }
 return str2.join("");
-};
+}
 module.exports=incrementMessage;

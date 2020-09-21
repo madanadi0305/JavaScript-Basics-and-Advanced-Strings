8d8dea737bc5e832de4d6375ab134ae043ebe243
madanadi0305 <madanadi0305@gmail.com>
2020-09-21 17:18:12 +0000

    Update task23.js

--- task23.js
+++ task23.js
@@ -4,6 +4,7 @@ var dup=number;
  for(var i=0;i<string.length;i++){
 //var k=string[i];]
 var ascii_code=string.charCodeAt(i)+number;
+var char_code=string.charCodeAt(i);
 if((ascii_code>=97 && ascii_code<=122)||(ascii_code<=90 && ascii_code>=65)){
 str2.push(String.fromCharCode(ascii_code));
 }  
@@ -11,7 +12,7 @@ str2.push(String.fromCharCode(ascii_code));
    str2.push(" ");
    }
  else if(ascii_code>=122){
-  dup=dup-(122-ascii_code);
+  dup=dup-(122-char_code);
   dup=dup%26;
    str2.push(String.fromCharCode(dup+96));
  }

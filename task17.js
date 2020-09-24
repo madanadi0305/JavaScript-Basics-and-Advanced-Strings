9b99c8a47cced0a72d09544f9683d59b0607938e
madanadi0305 <madanadi0305@gmail.com>
2020-09-24 13:37:11 +0000

    Update task17.js

--- task17.js
+++ task17.js
@@ -1,16 +1,26 @@
-function string_endsWith(str, suffix) 
-{
- 
- if (((str===null) || (str==='')) || ((suffix===null) || (suffix==='')))
-   {
-    return false;
-   }
-  else
-  {     
-   str = str.toString();
-   suffix = suffix.toString();
-   }
- return str.indexOf(suffix, str.length -     suffix.length) !== -1;
+function endsWithSubstring(string,substring){
+var string1=string.split(" ");
+  var str2=[];
+  var is_Found;
+  if(string===null||string===""||substring===null||substring===""){
+  return false;
+  }
+  var n=string.length;
+  var k=substring.length;
+  for(var j=(n-1);j>=(n-k);j--){
+  str2.push(string[j]);
+  }
+  var str3=str2.reverse().join("");
+  //console.log(string);
+  //console.log(str3);
+if((str3.toLowerCase()===substring.toLowerCase())||(str3.toUpperCase()===substring.toUpperCase())){
+return true;
+}
+  else{
+  return false;
+  }
 }
 
-module.exports=string_endsWith;
+//var a=endsWithSubstring("JS PHP PYTHON","PHP python");
+//console.log(a);
+module.exports=endsWithSubstring;

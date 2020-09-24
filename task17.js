209f06268ff5f88093de63444cb26bfda78439c1
madanadi0305 <madanadi0305@gmail.com>
2020-09-24 12:57:23 +0000

    Update Wiki/29-task17.md, task17.js files

--- task17.js
+++ task17.js
@@ -1,40 +1,16 @@
- function endsWithSubstring(string,substring){
-var string1=string.split(" ");
-  var str2=[];
-  var is_Found;
-  if(string===null||string===""||substring===null||substring===""){
-  return false;
-  }
-  
-  var n=string1.length;
-for(var i=0;i<n;i++){
-is_Found=0;
-  if(i===n-1){
-var k=string1[i];
-var last_index=k.length-1;
-  
-for(var j=last_index;j>(last_index-substring.length);j--)  
-{str2.push(k[j]);
-}
-}
-
-  var str3=str2.reverse().join("");
-
-   if(str3.toUpperCase()===substring.toUpperCase()||str3.toLowerCase()===substring.toLowerCase()){
-   is_Found=1;
-   break;
+function string_endsWith(str, suffix) 
+{
+ 
+ if (((str===null) || (str==='')) || ((suffix===null) || (suffix==='')))
+   {
+    return false;
    }
-  
-} 
- if(is_Found===1){
- return true;
- } 
-else{
-return false;
+  else
+  {     
+   str = str.toString();
+   suffix = suffix.toString();
+   }
+ return str.indexOf(suffix, str.length -     suffix.length) !== -1;
 }
-  
 
-}
-module.exports=endsWithSubstring;
-
-
+module.exports=string_endsWith;

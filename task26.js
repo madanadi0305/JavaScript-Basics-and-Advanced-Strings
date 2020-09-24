d0394643f7c0684befce7bcbb40328a9dfde9101
madanadi0305 <madanadi0305@gmail.com>
2020-09-24 09:30:11 +0000

    Update task26.js

--- task26.js
+++ task26.js
@@ -1,8 +1,9 @@
 function rotate(string,rotation_matrix){
 var split_matrix=rotation_matrix.split(" ");
 //console.log(split_matrix);
-var n=string.length;  
-var rotated_string;
+var n=string.length;
+var string1=string;  
+var rotated_string,min,TEMP,is_Anagram;
 var FIRSTCHARSTRING="";  
   for(var i=0;i<split_matrix.length;i++){
   if(i%2===0){
@@ -20,7 +21,37 @@ var FIRSTCHARSTRING="";
 string=rotated_string;
     
   }
-  return FIRSTCHARSTRING;
+  for(var i=0;i<FIRSTCHARSTRING.length;i++){
+    min=i;
+   for(var j=i+1;j<FIRSTCHARSTRING.length;j++) 
+   {if(FIRSTCHARSTRING[j]<FIRSTCHARSTRING[min]){
+     min=j;
+   }
+   }
+   if(min!=i){
+    TEMP=FIRSTCHARSTRING[min]; 
+    FIRSTCHARSTRING[min]=FIRSTCHARSTRING[i];
+    FIRSTCHARSTRING[i]=TEMP;
+
+   }
+  }
+
+  for(var i=0;i<string1.length;i++){
+    is_Anagram=0;
+   if(string1.indexOf(FIRSTCHARSTRING)!==-1){
+   is_Anagram=1;
+
+   }
+
+  }
+  if(is_Anagram===1){
+   return true;
+
+  }
+  else{
+  return false;
+
+  }
 }
 module.exports=rotate;
 //var a=rotate("abcde","L 3 R 2 R 4");
